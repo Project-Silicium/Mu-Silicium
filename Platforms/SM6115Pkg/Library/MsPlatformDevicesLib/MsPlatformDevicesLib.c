134aa0436f8be6ab20ec1afdba4d46350d0d79ca
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2023-01-06 16:40:10 +0100

    Upload SM6115 Sourcecode

---
+++ Platforms/SM6115Pkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
@@ -0,0 +1,133 @@
+/** @file
+ *MsPlatformDevicesLib  - Device specific library.
+Copyright (C) Microsoft Corporation. All rights reserved.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <Uefi.h>
+
+#include <Library/DebugLib.h>
+#include <Library/DeviceBootManagerLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/MsPlatformDevicesLib.h>
+#include <Library/PrintLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiRuntimeServicesTableLib.h>
+
+#include <Library/RFSProtectionLib.h>
+#include <Configuration/BootDevices.h>
+
+//
+// Predefined platform default console device path
+//
+BDS_CONSOLE_CONNECT_ENTRY gPlatformConsoles[] =
+{
+  {
+    (EFI_DEVICE_PATH_PROTOCOL *)&KeypadDevicePath,
+    CONSOLE_IN
+  },
+  {
+    (EFI_DEVICE_PATH_PROTOCOL *)&DisplayDevicePath,
+    CONSOLE_OUT | STD_ERROR
+  },
+  {
+    NULL,
+    0
+  }
+};
+
+EFI_DEVICE_PATH_PROTOCOL *gPlatformConInDeviceList[] = {NULL};
+
+/**
+Library function used to provide the platform SD Card device path
+**/
+EFI_DEVICE_PATH_PROTOCOL *EFIAPI GetSdCardDevicePath(VOID) { return NULL; }
+
+/**
+  Library function used to determine if the DevicePath is a valid bootable 'USB'
+device. USB here indicates the port connection type not the device protocol.
+  With TBT or USB4 support PCIe storage devices are valid 'USB' boot options.
+**/
+BOOLEAN
+EFIAPI
+PlatformIsDevicePathUsb(IN EFI_DEVICE_PATH_PROTOCOL *DevicePath)
+{
+  return FALSE;
+}
+
+/**
+Library function used to provide the list of platform devices that MUST be
+connected at the beginning of BDS
+**/
+EFI_DEVICE_PATH_PROTOCOL **EFIAPI GetPlatformConnectList(VOID) { return NULL; }
+
+EFI_DEVICE_PATH_PROTOCOL **EFIAPI
+GetDevicePathsFromProtocolGuid(IN EFI_GUID *ProtocolGuid, OUT UINTN *Count)
+{
+  EFI_STATUS                 Status;
+  EFI_HANDLE                *Handles;
+  UINTN                      NoHandles;
+  UINTN                      Idx;
+  EFI_DEVICE_PATH_PROTOCOL **pDevicePaths;
+  EFI_DEVICE_PATH_PROTOCOL  *pDevicePath;
+
+  Status = gBS->LocateHandleBuffer(
+      ByProtocol, ProtocolGuid, NULL /* SearchKey */, &NoHandles, &Handles);
+  if (EFI_ERROR(Status)) {
+    //
+    // This is not an error, just an informative condition.
+    //
+    DEBUG((EFI_D_VERBOSE, "%a: %g: %r\n", __FUNCTION__, ProtocolGuid, Status));
+    return NULL;
+  }
+
+  ASSERT(NoHandles > 0);
+
+  pDevicePaths = (EFI_DEVICE_PATH_PROTOCOL **)AllocateZeroPool(
+      (NoHandles + 1) * sizeof(EFI_DEVICE_PATH_PROTOCOL *));
+
+  *Count = 0;
+  for (Idx = 0; Idx < NoHandles; ++Idx) {
+    pDevicePath = DevicePathFromHandle(Handles[Idx]);
+    if (pDevicePath != NULL) {
+      *(pDevicePaths + *Count) = pDevicePath;
+      *Count                   = *Count + 1;
+    }
+  }
+
+  gBS->FreePool(Handles);
+
+  return pDevicePaths;
+}
+
+/**
+ * Library function used to provide the list of platform console devices.
+ */
+BDS_CONSOLE_CONNECT_ENTRY *EFIAPI GetPlatformConsoleList(VOID)
+{
+  return (BDS_CONSOLE_CONNECT_ENTRY *)&gPlatformConsoles;
+}
+
+/**
+Library function used to provide the list of platform devices that MUST be
+connected to support ConsoleIn activity.  This call occurs on the ConIn connect
+event, and allows platforms to do enable specific devices ConsoleIn support.
+**/
+EFI_DEVICE_PATH_PROTOCOL **EFIAPI GetPlatformConnectOnConInList(VOID)
+{
+  return NULL;
+}
+
+/**
+Library function used to provide the console type.  For ConType == DisplayPath,
+device path is filled in to the exact controller to use.  For other ConTypes,
+DisplayPath must NULL. The device path must NOT be freed.
+**/
+EFI_HANDLE
+EFIAPI
+GetPlatformPreferredConsole(OUT EFI_DEVICE_PATH_PROTOCOL **DevicePath)
+{
+  return NULL;
+}
---
+++ Platforms/SM6115Pkg/SM6115.dsc
@@ -0,0 +1,61 @@
+## @file
+#
+#  Copyright (c) 2011-2015, ARM Limited. All rights reserved.
+#  Copyright (c) 2014, Linaro Limited. All rights reserved.
+#  Copyright (c) 2015 - 2016, Intel Corporation. All rights reserved.
+#  Copyright (c) 2018 - 2019, Bingxing Wang. All rights reserved.
+#  Copyright (c) 2022, Xilin Wu. All rights reserved.
+#
+#  SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+##
+
+################################################################################
+#
+# Defines Section - statements that will be processed to create a Makefile.
+#
+################################################################################
+[Defines]
+  PLATFORM_NAME                  = SM6115
+  PLATFORM_GUID                  = b6325ac2-9f3f-4b1d-b129-ac7b35ddde60
+  PLATFORM_VERSION               = 0.1
+  DSC_SPECIFICATION              = 0x00010005
+  OUTPUT_DIRECTORY               = Build/SM6115Pkg
+  SUPPORTED_ARCHITECTURES        = AARCH64
+  BUILD_TARGETS                  = DEBUG|RELEASE
+  SKUID_IDENTIFIER               = DEFAULT
+  FLASH_DEFINITION               = SM6115Pkg/SM6115.fdf
+  USE_DISPLAYDXE                 = FALSE
+  SWITCHSLOT_SUPPORT             = FALSE
+  MASS_STORAGE_SUPPORT           = FALSE
+
+[BuildOptions.common]
+  *_*_*_CC_FLAGS = -D DISABLE_NEW_DEPRECATED_INTERFACES -D HAS_MEMCPY_INTRINSICS
+
+!include SM6115Pkg/Devices/$(TARGET_DEVICE)/$(TARGET_DEVICE).dsc.inc
+!include QcomPkg/Qcom.dsc.inc
+
+[PcdsFixedAtBuild.common]
+  gArmTokenSpaceGuid.PcdArmArchTimerFreqInHz|19200000
+  gArmTokenSpaceGuid.PcdArmArchTimerSecIntrNum|17
+  gArmTokenSpaceGuid.PcdArmArchTimerIntrNum|18
+  gArmTokenSpaceGuid.PcdGicDistributorBase|0xf200000
+  gArmTokenSpaceGuid.PcdGicRedistributorsBase|0xf300000
+
+  gEfiMdeModulePkgTokenSpaceGuid.PcdAcpiDefaultOemRevision|0x00006115
+  gEmbeddedTokenSpaceGuid.PcdInterruptBaseAddress|0xf300000
+  gEmbeddedTokenSpaceGuid.PcdPrePiCpuIoSize|44
+
+  gArmPlatformTokenSpaceGuid.PcdCoreCount|8
+  gArmPlatformTokenSpaceGuid.PcdClusterCount|2
+
+[LibraryClasses.common]
+  PlatformMemoryMapLib|SM6115Pkg/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.inf
+  PlatformPeiLib|SM6115Pkg/Library/PlatformPei/PlatformPeiLib.inf
+  PlatformPrePiLib|SM6115Pkg/Library/PlatformPrePiLib/PlatformPrePiLib.inf
+  MsPlatformDevicesLib|SM6115Pkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.inf
+  SOCSmbiosInfoLib|SM6115Pkg/Library/SOCSmbiosInfoLib/SOCSmbiosInfoLib.inf
+  RFSProtectionLib|SM6115Pkg/Library/RFSProtectionLib/RFSProtectionLib.inf
+
+[Components.AARCH64]
+  DfciPkg/AuthManagerNull/AuthManagerNull.inf
