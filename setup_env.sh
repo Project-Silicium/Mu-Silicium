f13b5592c1be4da71d9d9e7d91508e3f2cf33e11
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2023-01-10 18:31:24 +0100

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install Dependence
+sudo apt update && sudo apt -y upgrade
+sudo apt -y install python3-venv pip git mono-devel build-essential nuget build-essential uuid-dev iasl nasm gcc-aarch64-linux-gnu python3 python3-distutils python3-git python3-pip gettext locales gnupg ca-certificates python3-venv git git-core clang llvm curl
+
 # Clang LLVM
 sudo apt install -y clang llvm gcc-aarch64-linux-gnu
 
