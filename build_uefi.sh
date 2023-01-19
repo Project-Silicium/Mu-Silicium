6650cca9fd17e5a96194fa128366c28fa31b2b83
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2023-01-19 15:21:12 +0100

    Update build_uefi.sh

--- build_uefi.sh
+++ build_uefi.sh
@@ -8,19 +8,22 @@ function _help(){
 	echo "Options:"
 	echo "	--device DEV, -d DEV:    build for DEV."
 	echo "	--release MODE, -r MODE: Release mode for building, default is 'RELEASE', 'DEBUG' alternatively."
-	echo "        --help, -h:              Shows this Help."
+	echo "	--help, -h:              Shows this Help."
+	echo "	--memory -m:             Define how much Memory your Device has."
 	echo
-	echo "MainPage: https://github.com/Robotix22/MU-msm"
+	echo "MainPage: https://github.com/Robotix22/MU-Qcom"
 	exit 1
 }
 
 function _error(){ echo "${@}" >&2;exit 1; }
 
 TARGET_DEVICE=""
+TARGET_MEM_SIZE=""
+MULTIPLE_MEM_SIZE="FALSE"
 TARGET_BUILD_MODE="RELEASE"
 UNTESTED_DEVICE=""
 IGNORE_WARNING=""
-OPTS="$(getopt -o d:hfabcACDO:r:ifabcACDO: -l device:,help,ignore,release: -n 'build_uefi.sh' -- "$@")"||exit 1
+OPTS="$(getopt -o d:hfabcACDO:r:ifabcACDO:m: -l device:,help,ignore,release:,memory: -n 'build_uefi.sh' -- "$@")"||exit 1
 eval set -- "${OPTS}"
 while true
 do	case "${1}" in
@@ -28,6 +31,7 @@ do	case "${1}" in
 		-h|--help) _help 0;shift;;
 		-r|--release) TARGET_BUILD_MODE="${2}";shift 2;;
 		-i|--ignore) IGNORE_WARNING="TRUE";shift;;
+		-m|--memory) TARGET_MEM_SIZE="${2}";shift 2;;
 		--) shift;break;;
 		*) _help 1;;
 	esac
@@ -65,6 +69,19 @@ if [ -z ${UNTESTED_DEVICE} ]; then echo ""; else
     exit 1
 fi
 
+if [ -z ${TARGET_MEM_SIZE} ]; then
+if [ ${MULTIPLE_MEM_SIZE} = TRUE ]; then
+    echo "=================[ WARNING ]================="
+    echo "The Device you chose has more than one Mem Size!"
+    echo "Use -m or --memory to define how much Mem your Device has."
+    echo "============================================="
+    echo ""
+    exit 1
+else echo "";
+fi
+else echo "";
+fi
+
 rm ./BootShim/BootShim.bin
 rm ./BootShim/BootShim.elf
 rm ./ImageResources/bootpayload.bin
@@ -78,7 +95,7 @@ cd ..
 
 stuart_setup -c "Platforms/${SOC_PLATFORM}Pkg/PlatformBuild.py" TOOL_CHAIN_TAG=CLANG38||exit 1
 stuart_update -c "Platforms/${SOC_PLATFORM}Pkg/PlatformBuild.py" TOOL_CHAIN_TAG=CLANG38||exit 1
-stuart_build -c "Platforms/${SOC_PLATFORM}Pkg/PlatformBuild.py" TOOL_CHAIN_TAG=CLANG38 "TARGET=${_TARGET_BUILD_MODE}" "TARGET_DEVICE=${TARGET_DEVICE}" "FD_BASE=${FD_BASE}" "FD_SIZE=${FD_SIZE}"||exit 1
+stuart_build -c "Platforms/${SOC_PLATFORM}Pkg/PlatformBuild.py" TOOL_CHAIN_TAG=CLANG38 "TARGET=${_TARGET_BUILD_MODE}" "TARGET_DEVICE=${TARGET_DEVICE}" "FD_BASE=${FD_BASE}" "FD_SIZE=${FD_SIZE}" "MEM_SIZE=${TARGET_MEM_SIZE}"||exit 1
 
 cat ./BootShim/BootShim.bin "./Build/${SOC_PLATFORM}Pkg/${_TARGET_BUILD_MODE}_CLANG38/FV/${SOC_PLATFORM}PKG_UEFI.fd" > "./Build/${SOC_PLATFORM}Pkg/${_TARGET_BUILD_MODE}_CLANG38/FV/${SOC_PLATFORM}PKG_UEFI.fd-bootshim" ||exit 1
 gzip -c < "./Build/${SOC_PLATFORM}Pkg/${_TARGET_BUILD_MODE}_CLANG38/FV/${SOC_PLATFORM}PKG_UEFI.fd-bootshim" > "./Build/${SOC_PLATFORM}Pkg/${_TARGET_BUILD_MODE}_CLANG38/FV/${SOC_PLATFORM}PKG_UEFI.fd-bootshim.gz" ||exit 1
