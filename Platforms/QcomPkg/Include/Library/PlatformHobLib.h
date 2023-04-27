b8755f66aa4ab2c21d889fbf7d9f2965644791dc
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2023-04-27 13:52:32 +0200

    Delete PlatformHobLib.h

--- Platforms/QcomPkg/Include/Library/PlatformHobLib.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PLATFORM_HOB_LIB_LIB_H_
-#define _PLATFORM_HOB_LIB_LIB_H_
-
-#include <Configuration/XblHlosHob.h>
-
-PXBL_HLOS_HOB GetPlatformHob();
-
-#endif /* _PLATFORM_HOB_LIB_LIB_H_ */
