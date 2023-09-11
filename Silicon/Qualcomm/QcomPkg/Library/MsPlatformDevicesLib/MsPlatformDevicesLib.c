6dc038fbb5595cc66469f8b3fb059ccc205e82b1
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2023-09-11 17:30:31 +0200

    Qcom: Start USB Controller before BDS
    Origin: https://github.com/WOA-Project/SurfaceDuoPkg/commit/f28f1e4fb4ae08dddecd87452b9eb38b55033e57

--- Silicon/Qualcomm/QcomPkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
+++ Silicon/Qualcomm/QcomPkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
@@ -76,13 +76,40 @@ PlatformIsDevicePathUsb(IN EFI_DEVICE_PATH_PROTOCOL *DevicePath)
   return FALSE;
 }
 
+/* Dummy function needed for event notification callback */
+STATIC VOID DummyNotify(IN EFI_EVENT Event, IN VOID *Context) {}
+
 /**
 Library function used to provide the list of platform devices that MUST be
 connected at the beginning of BDS
 **/
 EFI_DEVICE_PATH_PROTOCOL **EFIAPI GetPlatformConnectList(VOID)
 {
+  EFI_STATUS Status;
+  EFI_EVENT  UsbConfigEvt;
+  EFI_GUID   InitUsbControllerGuid = {
+      0x1c0cffce,
+      0xfc8d,
+      0x4e44,
+      {0x8c, 0x78, 0x9c, 0x9e, 0x5b, 0x53, 0xd, 0x36}};
+
+  // Update the ACPI tables with SOC runtime information
   PlatformUpdateAcpiTables();
+
+  // Notify the USB controller to start now
+  Status = gBS->CreateEventEx(
+      EVT_NOTIFY_SIGNAL, TPL_CALLBACK, DummyNotify, NULL,
+      &InitUsbControllerGuid, &UsbConfigEvt);
+  if (EFI_ERROR(Status)) {
+    DEBUG(
+        (EFI_D_ERROR, "Usb controller init event not signaled: %r\n", Status));
+  }
+  else {
+    gBS->SignalEvent(UsbConfigEvt);
+    gBS->CloseEvent(UsbConfigEvt);
+  }
+
+  // We do not have any device to advertise for connection before BDS begins
   return NULL;
 }
 
