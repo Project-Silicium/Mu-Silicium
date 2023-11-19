649d66a763b41dce56188c5b79b564618c1d2e2a
AistopGit <oleg18072008t@gmail.com>
2023-11-19 18:07:13 +0300

    nitrogen: add initial support for Xiaomi Mi Max 3

---
+++ Silicon/Qualcomm/SDM660Pkg/Library/PlatformPeiLib/PlatformPeiLibInternal.h
@@ -0,0 +1,18 @@
+#ifndef _PLATFORM_HOB_INTERNAL_H_
+#define _PLATFORM_HOB_INTERNAL_H_
+
+#include <Library/ArmLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/HobLib.h>
+#include <Library/IoLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PcdLib.h>
+#include <Library/SerialPortLib.h>
+#include <Library/MemoryMapHelperLib.h>
+#include <Library/PlatformHobs.h>
+
+// This varies by device
+#include <Configuration/DeviceConfigurationMap.h>
+
+#endif // _PLATFORM_HOB_INTERNAL_H_
