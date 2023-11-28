637c1b033cfe1c2e426373e01915f0f12767ef6f
d4n1 <d4n1.551@gmail.com>
2023-11-28 23:31:45 +0500

    docker: adjust

--- docker-build.sh
+++ docker-build.sh
@@ -2,9 +2,11 @@
 
 REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-VERSION=$(git --no-pager log -n 1 --pretty=format:"%H" -- Dockerfile)
-IMG_NAME="edk2builder:$VERSION"
+cd $REPO
 
-docker build -t "$IMG_NAME" .docker
+VERSION=$(git --no-pager log -n 1 --pretty=format:"%H" -- .docker/Dockerfile)
+IMG_NAME="mu_builder:$VERSION"
+
+docker build -t "$IMG_NAME" .
 
 docker run --rm -it -v $REPO:/app "$IMG_NAME" /runasuser.sh --uid $(id -u) --command "./build_uefi.sh $@"
