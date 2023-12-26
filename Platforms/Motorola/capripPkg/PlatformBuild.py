52ec347fab2b202941d1ddaebaf3b63984341909
Vicente <vicenteicc2008@gmail.com>
2023-12-26 14:35:51 -0300

    add Moto G30 initial support

---
+++ Platforms/Motorola/capripPkg/PlatformBuild.py
@@ -0,0 +1,258 @@
+# @file
+# Script to Build Motorola Moto G30 UEFI firmware
+#
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+import datetime
+import logging
+import os
+import uuid
+from io import StringIO
+from pathlib import Path
+
+from edk2toolext.environment import shell_environment
+from edk2toolext.environment.uefi_build import UefiBuilder
+from edk2toolext.invocables.edk2_platform_build import BuildSettingsManager
+from edk2toolext.invocables.edk2_pr_eval import PrEvalSettingsManager
+from edk2toolext.invocables.edk2_setup import (RequiredSubmodule,
+                                               SetupSettingsManager)
+from edk2toolext.invocables.edk2_update import UpdateSettingsManager
+from edk2toolext.invocables.edk2_parse import ParseSettingsManager
+from edk2toollib.utility_functions import RunCmd
+
+    # ####################################################################################### #
+    #                                Common Configuration                                     #
+    # ####################################################################################### #
+class CommonPlatform():
+    ''' Common settings for this platform.  Define static data here and use
+        for the different parts of stuart
+    '''
+    PackagesSupported = ("capripPkg")
+    ArchSupported = ("AARCH64")
+    TargetsSupported = ("DEBUG", "RELEASE")
+    Scopes = ('caprip', 'gcc_aarch64_linux', 'edk2-build')
+    WorkspaceRoot = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    PackagesPath = (
+        "Platforms/Motorola",
+        "Common/Mu",
+        "Common/Mu_OEM_Sample",
+        "Common/Mu_Tiano_Plus",
+        "Features/DFCI",
+        "Mu_Basecore",
+        "Silicon/Arm/Mu_Tiano",
+        "Silicon/Qualcomm"
+    )
+
+
+    # ####################################################################################### #
+    #                         Configuration for Update & Setup                                #
+    # ####################################################################################### #
+class SettingsManager(UpdateSettingsManager, SetupSettingsManager, PrEvalSettingsManager, ParseSettingsManager):
+
+    def GetPackagesSupported(self):
+        ''' return iterable of edk2 packages supported by this build.
+        These should be edk2 workspace relative paths '''
+        return CommonPlatform.PackagesSupported
+
+    def GetArchitecturesSupported(self):
+        ''' return iterable of edk2 architectures supported by this build '''
+        return CommonPlatform.ArchSupported
+
+    def GetTargetsSupported(self):
+        ''' return iterable of edk2 target tags supported by this build '''
+        return CommonPlatform.TargetsSupported
+
+    def GetRequiredSubmodules(self):
+        """Return iterable containing RequiredSubmodule objects.
+
+        !!! note
+            If no RequiredSubmodules return an empty iterable
+        """
+        return [
+            RequiredSubmodule("Binaries", True),
+            RequiredSubmodule("Common/Mu", True),
+            RequiredSubmodule("Common/Mu_OEM_Sample", True),
+            RequiredSubmodule("Common/Mu_Tiano_Plus", True),
+            RequiredSubmodule("Features/DFCI", True),
+            RequiredSubmodule("Mu_Basecore", True),
+            RequiredSubmodule("Silicon/Arm/Mu_Tiano", True),
+        ]
+
+    def SetArchitectures(self, list_of_requested_architectures):
+        ''' Confirm the requests architecture list is valid and configure SettingsManager
+        to run only the requested architectures.
+
+        Raise Exception if a list_of_requested_architectures is not supported
+        '''
+        unsupported = set(list_of_requested_architectures) - \
+            set(self.GetArchitecturesSupported())
+        if(len(unsupported) > 0):
+            errorString = (
+                "Unsupported Architecture Requested: " + " ".join(unsupported))
+            logging.critical( errorString )
+            raise Exception( errorString )
+        self.ActualArchitectures = list_of_requested_architectures
+
+    def GetWorkspaceRoot(self):
+        ''' get WorkspacePath '''
+        return CommonPlatform.WorkspaceRoot
+
+    def GetActiveScopes(self):
+        ''' return tuple containing scopes that should be active for this process '''
+        return CommonPlatform.Scopes
+
+    def FilterPackagesToTest(self, changedFilesList: list, potentialPackagesList: list) -> list:
+        ''' Filter other cases that this package should be built
+        based on changed files. This should cover things that can't
+        be detected as dependencies. '''
+        build_these_packages = []
+        possible_packages = potentialPackagesList.copy()
+        for f in changedFilesList:
+            # BaseTools files that might change the build
+            if "BaseTools" in f:
+                if os.path.splitext(f) not in [".txt", ".md"]:
+                    build_these_packages = possible_packages
+                    break
+
+            # if the azure pipeline platform template file changed
+            if "platform-build-run-steps.yml" in f:
+                build_these_packages = possible_packages
+                break
+
+        return build_these_packages
+
+    def GetPlatformDscAndConfig(self) -> tuple:
+        ''' If a platform desires to provide its DSC then Policy 4 will evaluate if
+        any of the changes will be built in the dsc.
+
+        The tuple should be (<workspace relative path to dsc file>, <input dictionary of dsc key value pairs>)
+        '''
+        return ("capripPkg/capripNoSb.dsc", {})
+
+    def GetName(self):
+        return "caprip"
+
+    def GetPackagesPath(self):
+        ''' Return a list of paths that should be mapped as edk2 PackagesPath '''
+        return CommonPlatform.PackagesPath
+
+    # ####################################################################################### #
+    #                         Actual Configuration for Platform Build                         #
+    # ####################################################################################### #
+class PlatformBuilder(UefiBuilder, BuildSettingsManager):
+    def __init__(self):
+        UefiBuilder.__init__(self)
+
+    def AddCommandLineOptions(self, parserObj):
+        ''' Add command line options to the argparser '''
+
+        # In an effort to support common server based builds this parameter is added.  It is
+        # checked for correctness but is never uses as this platform only supports a single set of
+        # architectures.
+        parserObj.add_argument('-a', "--arch", dest="build_arch", type=str, default="AARCH64",
+            help="Optional - CSV of architecture to build.  AARCH64 is used for PEI and "
+            "DXE and is the only valid option for this platform.")
+
+    def RetrieveCommandLineOptions(self, args):
+        '''  Retrieve command line options from the argparser '''
+        if args.build_arch.upper() != "AARCH64":
+            raise Exception("Invalid Arch Specified.  Please see comments in PlatformBuild.py::PlatformBuilder::AddCommandLineOptions")
+
+    def GetWorkspaceRoot(self):
+        ''' get WorkspacePath '''
+        return CommonPlatform.WorkspaceRoot
+
+    def GetPackagesPath(self):
+        ''' Return a list of paths that should be mapped as edk2 PackagesPath '''
+        result = [
+            shell_environment.GetBuildVars().GetValue("FEATURE_CONFIG_PATH", "")
+        ]
+        for a in CommonPlatform.PackagesPath:
+            result.append(a)
+        return result
+
+    def GetActiveScopes(self):
+        ''' return tuple containing scopes that should be active for this process '''
+        return CommonPlatform.Scopes
+
+    def GetName(self):
+        ''' Get the name of the repo, platform, or product being build '''
+        ''' Used for naming the log file, among others '''
+        return "capripPkg"
+
+    def GetLoggingLevel(self, loggerType):
+        """Get the logging level depending on logger type.
+
+        Args:
+            loggerType (str): type of logger being logged to
+
+        Returns:
+            (Logging.Level): The logging level
+
+        !!! note "loggerType possible values"
+            "base": lowest logging level supported
+
+            "con": logs to screen
+
+            "txt": logs to plain text file
+        """
+        return logging.INFO
+        return super().GetLoggingLevel(loggerType)
+
+    def SetPlatformEnv(self):
+        logging.debug("PlatformBuilder SetPlatformEnv")
+        self.env.SetValue("PRODUCT_NAME", "caprip", "Platform Hardcoded")
+        self.env.SetValue("ACTIVE_PLATFORM", "capripPkg/caprip.dsc", "Platform Hardcoded")
+        self.env.SetValue("TARGET_ARCH", "AARCH64", "Platform Hardcoded")
+        self.env.SetValue("TOOL_CHAIN_TAG", "CLANG38", "set default to clang38")
+        self.env.SetValue("EMPTY_DRIVE", "FALSE", "Default to false")
+        self.env.SetValue("RUN_TESTS", "FALSE", "Default to false")
+        self.env.SetValue("SHUTDOWN_AFTER_RUN", "FALSE", "Default to false")
+        # needed to make FV size build report happy
+        self.env.SetValue("BLD_*_BUILDID_STRING", "Unknown", "Default")
+        # Default turn on build reporting.
+        self.env.SetValue("BUILDREPORTING", "TRUE", "Enabling build report")
+        self.env.SetValue("BUILDREPORT_TYPES", "PCD DEPEX FLASH BUILD_FLAGS LIBRARY FIXED_ADDRESS HASH", "Setting build report types")
+        self.env.SetValue("BLD_*_MEMORY_PROTECTION", "TRUE", "Default")
+        # Include the MFCI test cert by default, override on the commandline with "BLD_*_SHIP_MODE=TRUE" if you want the retail MFCI cert
+        self.env.SetValue("BLD_*_SHIP_MODE", "FALSE", "Default")
+        self.env.SetValue("BLD_*_RAM_SIZE", self.env.GetValue("RAM_SIZE"), "Default")
+        self.env.SetValue("BLD_*_FD_BASE", self.env.GetValue("FD_BASE"), "Default")
+        self.env.SetValue("BLD_*_FD_SIZE", self.env.GetValue("FD_SIZE"), "Default")
+        self.env.SetValue("BLD_*_FD_BLOCKS", self.env.GetValue("FD_BLOCKS"), "Default")
+        return 0
+
+    def PlatformPreBuild(self):
+        return 0
+
+    def PlatformPostBuild(self):
+        return 0
+
+    def FlashRomImage(self):
+        return 0
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    from edk2toolext.invocables.edk2_platform_build import Edk2PlatformBuild
+    from edk2toolext.invocables.edk2_setup import Edk2PlatformSetup
+    from edk2toolext.invocables.edk2_update import Edk2Update
+    SCRIPT_PATH = os.path.relpath(__file__)
+    parser = argparse.ArgumentParser(add_help=False)
+    parse_group = parser.add_mutually_exclusive_group()
+    parse_group.add_argument("--update", "--UPDATE",
+                             action='store_true', help="Invokes stuart_update")
+    parse_group.add_argument("--setup", "--SETUP",
+                             action='store_true', help="Invokes stuart_setup")
+    args, remaining = parser.parse_known_args()
+    new_args = ["stuart", "-c", SCRIPT_PATH]
+    new_args = new_args + remaining
+    sys.argv = new_args
+    if args.setup:
+        Edk2PlatformSetup().Invoke()
+    elif args.update:
+        Edk2Update().Invoke()
+    else:
+        Edk2PlatformBuild().Invoke()
