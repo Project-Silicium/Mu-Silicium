4e917cda6d1efd8f7aed2abecfd4545029e8e4cc
Vicente <vicenteicc2008@gmail.com>
2023-12-26 16:52:39 -0300

    fix memory map

--- Platforms/Motorola/capripPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
+++ Platforms/Motorola/capripPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
@@ -10,6 +10,7 @@ gDeviceMemoryDescriptorEx[] = {
   {"Kernel",            0x40000000, 0x05700000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
   {"HYP",               0x45700000, 0x00600000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
   {"Boot Info",         0x45D00000, 0x00020000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
+  {"Boot Log",          0x45E11000, 0x00002000, AddMem, MEM_RES, UNCACHEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
   {"SMEM",              0x46000000, 0x00200000, AddMem, MEM_RES, WRITE_COMBINEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
   {"PIL Reserved",      0x4AB00000, 0x09400000, AddMem, MEM_RES, WRITE_COMBINEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
 //{"DXE Heap",          0x53F00000, 0x02800000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
@@ -23,7 +24,7 @@ gDeviceMemoryDescriptorEx[] = {
   {"CPU Vectors",       0x5FF8C000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
   {"MMU PageTables",    0x5FF8D000, 0x00003000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
   {"UEFI Stack",        0x5FF90000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
-  {"Log Buffer",        0x5FFF7000, 0x00008000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN},
+  {"Log Buffer",        0x5FFF0000, 0x0000F000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN},
   {"Info Blk",          0x5FFFF000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN},
   {"ramoops",           0x64000000, 0x00400000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN}, // pstore
   {"DXE Heap",          0x64400000, 0x04500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
