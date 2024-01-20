38f298293b6562c6b25859bf4a4cf4c777f22de5
statzar <statzar@HOME.Home>
2024-01-20 17:12:05 +0300

    Fix Memory Map

--- Platforms/Xiaomi/fogPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
+++ Platforms/Xiaomi/fogPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
@@ -41,9 +41,7 @@ gDeviceMemoryDescriptorEx[] = {
   // DDR Bank 0 end
 
   // DDR Bank 1 start
-  {"RAM Partition",       0x80000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
-
-  {"RAM Partition",		0xC0000000,0x80000000, AddMem,	SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
+  {"RAM Partition",       0x80000000, 0x80000000, AddMem,	SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
 //=================================================================================================================
 #elif DEVICE_RAM == 4
 //==================================================4GB RAM Setup==================================================
@@ -52,9 +50,7 @@ gDeviceMemoryDescriptorEx[] = {
   // DDR Bank 0 end
 
   // DDR Bank 1 start
-  {"RAM Partition",         0x80000000, 0x40000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
-
-  {"RAM Partition",         0xC0000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
+  {"RAM Partition",         0x80000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
 //=================================================================================================================
 #elif DEVICE_RAM == 6
 //==================================================6GB RAM Setup==================================================
@@ -63,9 +59,7 @@ gDeviceMemoryDescriptorEx[] = {
   // DDR Bank 0 end
 
   // DDR Bank 1 start
-  {"RAM Partition",         0x80000000, 0x40000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
-
-  {"RAM Partition",         0xC0000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
+  {"RAM Partition",         0x80000000, 0x100000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
 //=================================================================================================================
 #endif
 
