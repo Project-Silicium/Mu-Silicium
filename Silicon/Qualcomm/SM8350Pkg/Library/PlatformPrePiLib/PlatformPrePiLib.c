ab05a78ceb4063b30343751e9a0866af4142e44f
N1kroks <nikroksm@mail.ru>
2024-04-14 17:19:35 +0500

    Update KEP & Enable multicore for sm8350, sm7325

--- Silicon/Qualcomm/SM8350Pkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
+++ Silicon/Qualcomm/SM8350Pkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
@@ -8,5 +8,13 @@ VOID
 PlatformInitialize ()
 {
   // Initialize GIC
-  MmioWrite32 (GICR_WAKER_CURRENT_CPU, (MmioRead32 (GICR_WAKER_CURRENT_CPU) & ~GIC_WAKER_PROCESSORSLEEP));
+  // Wake up redistributor for CPU 0
+  MmioWrite32(
+      GICR_WAKER_CURRENT_CPU,
+      (MmioRead32(GICR_WAKER_CURRENT_CPU) & ~GIC_WAKER_PROCESSORSLEEP));
+
+  // Wake up redistributor for CPU 1
+  MmioWrite32(
+      GICR_WAKER_CPU(1),
+      (MmioRead32(GICR_WAKER_CPU(1)) & ~GIC_WAKER_PROCESSORSLEEP));
 }
