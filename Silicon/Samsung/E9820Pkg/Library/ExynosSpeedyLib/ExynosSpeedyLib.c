ef0b60c08be03c6eb26f6d5cd94cb7cfd6d5bd76
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2024-05-27 16:01:30 +0200

    Add Exynos Speedy Driver

---
+++ Silicon/Samsung/E9820Pkg/Library/ExynosSpeedyLib/ExynosSpeedyLib.c
@@ -0,0 +1,50 @@
+#include <Library/IoLib.h>
+#include <Library/TimerLib.h>
+#include <Library/ExynosSpeedyLib.h>
+
+STATIC
+VOID
+SWResetSpeedy (UINT32 SpeedyBase)
+{
+  UINT32 SpeedyCtrl;
+
+  // Set SpeedyCtrl Config
+  SpeedyCtrl  = MmioRead32 (SpeedyBase + SPEEDY_CTRL);
+  SpeedyCtrl |= SPEEDY_SW_RST;
+
+  // Write Speedy Config
+  MmioWrite32 (SpeedyBase + SPEEDY_CTRL, SpeedyCtrl);
+
+  // Wait a Bit
+  MicroSecondDelay (10);
+}
+
+STATIC
+VOID
+EnableSpeedy (UINT32 SpeedyBase)
+{
+  UINT32 SpeedyCtrl;
+
+  // Set SpeedyCtrl Config
+  SpeedyCtrl  = MmioRead32 (SpeedyBase + SPEEDY_CTRL);
+  SpeedyCtrl |= SPEEDY_ENABLE;
+
+  // Write SpeedyCtrl Config
+  MmioWrite32 (SpeedyBase + SPEEDY_CTRL, SpeedyCtrl);
+}
+
+VOID
+InitSpeedy ()
+{
+  // Clear Speedy Interrupt States
+  MmioWrite32 (0x15970000 + SPEEDY_INT_STATUS, 0xFFFFFFFF);
+  MmioWrite32 (0x15980000 + SPEEDY_INT_STATUS, 0xFFFFFFFF);
+
+  // Reset Speedy
+  SWResetSpeedy (0x15970000);
+  SWResetSpeedy (0x15980000);
+
+  // Enable Speedy
+  EnableSpeedy (0x15970000);
+  EnableSpeedy (0x15980000);
+}
