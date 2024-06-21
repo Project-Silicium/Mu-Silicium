2411e0cca1907cbaaa0e89d88f728ecb7b911c8f
2Petro <petronazehokok@gmail.com>
2024-06-21 04:11:48 +0300

    Fix boot issues
    Using gts8 binaries for now zeus one cause shell to crash and stuck

--- Resources/Scripts/zeus.sh
+++ Resources/Scripts/zeus.sh
@@ -3,10 +3,10 @@
 # Build an Android kernel that is actually UEFI disguised as the Kernel
 cat ./BootShim/AARCH64/BootShim.bin "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ZEUS_UEFI.fd" > "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim"||exit 1
 
-#gzip -c < "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim" > "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim.gz"||exit 1
-#cat "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim.gz" ./Resources/DTBs/zeus.dtb > ./Resources/bootpayload.bin||exit 1
+gzip -c < "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim" > "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim.gz"||exit 1
+cat "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim.gz" ./Resources/DTBs/zeus.dtb > ./Resources/bootpayload.bin||exit 1
 
-cp -r "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim" ./Resources/bootpayload.bin
+#cp -r "./Build/zeusPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/zeus_UEFI.fd-bootshim" ./Resources/bootpayload.bin
 
 
 # Create bootable Android boot.img
@@ -16,9 +16,10 @@ python3 ./Resources/Scripts/mkbootimg.py \
   --kernel_offset 0x00000000 \
   --ramdisk_offset 0x00000000 \
   --tags_offset 0x00000000 \
-  --os_version 13.0.0 \
-  --os_patch_level "$(date '+%Y-%m')" \
+  --os_version 12.0.0 \
+  --os_patch_level 2023-08 \
   --header_version 4 \
+  --pagesize 4096 \
   -o boot.img \
   ||_error "\nFailed to create Android Boot Image!\n"
 
