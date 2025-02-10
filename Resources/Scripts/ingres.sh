03278e19239848cfa76f44110b9676e5b69605b9
NikkaGames <nikagames111@gmail.com>
2025-02-11 00:01:38 +0400

    Fix boot script

--- Resources/Scripts/ingres.sh
+++ Resources/Scripts/ingres.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Build an Android kernel that is actually UEFI disguised as the Kernel
-cat ./BootShim/AARCH64/BootShim.bin "/home/nikka/Mu-Silicium/Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/INGRES_UEFI.fd" > "/home/nikka/Mu-Silicium/Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim"||exit 1
-gzip -c < "/home/nikka/Mu-Silicium/Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim" > "/home/nikka/Mu-Silicium/Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim.gz"||exit 1
-cat "/home/nikka/Mu-Silicium/Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim.gz" ./Resources/DTBs/ingres.dtb > ./Resources/bootpayload.bin||exit 1
+cat ./BootShim/AARCH64/BootShim.bin "./Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/INGRES_UEFI.fd" > "./Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim"||exit 1
+gzip -c < "./Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim" > "./Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim.gz"||exit 1
+cat "./Build/ingresPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/ingres_UEFI.fd-bootshim.gz" ./Resources/DTBs/ingres.dtb > ./Resources/bootpayload.bin||exit 1
 
 # Create bootable Android boot.img
 python3 ./Resources/Scripts/mkbootimg.py \
