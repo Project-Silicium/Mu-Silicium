206e34e46a8c930e7c195f2bd510dd9f72e2d85e
Glitchythedev <Glitchythedev@gmail.com>
2025-04-16 22:18:41 +0300

    Fixes

--- Platforms/Samsung/gtaxlltePkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
+++ Platforms/Samsung/gtaxlltePkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
@@ -9,7 +9,6 @@ gDeviceMemoryDescriptorEx[] = {
   {"CPU Vectors",        0x40008000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
   {"UEFI Stack",         0x40001000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
   {"RAM Partition",      0x40041000, 0x0000F000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
-  //{"FDT Pointer",        0x40050000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
   {"RAM Partition",      0x40051000, 0x103AF000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
   {"UEFI FD",            0x50400000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
   {"DXE Heap",           0x50600000, 0x03C00000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
@@ -22,7 +21,6 @@ gDeviceMemoryDescriptorEx[] = {
   {"Gic Redistributors", 0x104E2000, 0x00006000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
   {"Decon-F",            0x14830000 ,0x00008000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
   {"Pinctrl",            0x139F0000, 0x00001000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
-  //{"DDR Info",           0x0206B000, 0x0002B000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
   
   // Terminator for MMU
   {"Terminator", 0, 0, 0, 0, 0, 0, 0}
