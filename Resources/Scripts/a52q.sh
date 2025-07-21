c72934377918cdbdad20332acb158788ba5cc89a
Arminas Keršys <arminas@ver.lt>
2025-07-21 10:35:05 +0300

    a52q.sh: Add .tar output for Odin/Heimdall flash

--- Resources/Scripts/a52q.sh
+++ Resources/Scripts/a52q.sh
@@ -15,5 +15,9 @@ python3 ./Resources/Scripts/mkbootimg.py \
   --os_version 13.0.0 \
   --os_patch_level "$(date '+%Y-%m')" \
   --header_version 1 \
-  -o Mu-a52q.img \
+  -o boot.img \
   ||_error "\nFailed to create Android Boot Image!\n"
+
+# Compress Boot Image in a tar File for Odin/heimdall Flash
+tar -c boot.img -f Mu-a52q.tar||exit 1
+mv boot.img Mu-a52q.img||exit 1
