e32ca59f8e87296450ff1068167c7c9c49d88eac
alfaonyt <alex@pillou.net>
2025-08-03 18:13:34 +0200

    nuwa: Xiaomi 13 Pro Initial Support

---
+++ Platforms/Xiaomi/nuwaPkg/XBL-FdtBlob/README.md
@@ -0,0 +1,8 @@
+## DTB Infos
+
+- Platform: MTP
+- Version: `?` / `BOOT.MXF.2.1.1-00218-KAILUA-1.36233.1`
+
+## Changes
+
+No Changes yet.
---
+++ Resources/Scripts/nuwa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Package UEFI Image as Android Kernel
+cat ./BootShim/BootShim.bin "./Build/nuwaPkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/NUWA_UEFI.fd" > "./Build/nuwaPkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/NUWA_UEFI.fd-bootshim"||exit 1
+gzip -c < "./Build/nuwaPkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/NUWA_UEFI.fd-bootshim" > "./Build/nuwaPkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/NUWA_UEFI.fd-bootshim.gz"||exit 1
+cat "./Build/nuwaPkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/NUWA_UEFI.fd-bootshim.gz" ./Resources/DTBs/nuwa.dtb > ./Resources/bootpayload.bin||exit 1
+
+# Create Android boot.img
+python3 ./Resources/Scripts/mkbootimg.py \
+  --kernel ./Resources/bootpayload.bin \
+  --ramdisk ./Resources/ramdisk \
+  --os_version 15.0.0 \
+  --os_patch_level "$(date '+%Y-%m')" \
+  --header_version 4 \
+  -o Mu-nuwa.img \
+  ||_error "\nFailed to create Android Boot Image!\n"
