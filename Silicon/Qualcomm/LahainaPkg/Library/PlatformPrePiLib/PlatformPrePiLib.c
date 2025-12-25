3d693f2c118d9eb523781ccaa6357c8310d769ba
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2025-12-25 14:59:24 +0100

    Update PlatformPrePiLib

--- Silicon/Qualcomm/LahainaPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
+++ Silicon/Qualcomm/LahainaPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
@@ -1,7 +1,7 @@
 #include <Library/IoLib.h>
 #include <Library/PlatformPrePiLib.h>
-#include <Library/PcdLib.h>
 #include <Library/ConfigurationMapHelperLib.h>
+#include <Library/PcdLib.h>
 
 #include "PlatformRegisters.h"
 
@@ -12,11 +12,11 @@ PlatformInitialize ()
   UINT32     EarlyInitCoreCnt;
 
   // Get Early Cores Count
-  Status = LocateConfigurationMapUINT32ByName("EarlyInitCoreCnt", &EarlyInitCoreCnt);
+  Status = LocateConfigurationMapUINT32ByName ("EarlyInitCoreCnt", &EarlyInitCoreCnt);
   if (!EFI_ERROR (Status)) {
     // Wake Up all Cores
     for (UINTN i = 0; i < EarlyInitCoreCnt; i++) {
-      MmioWrite32 (GICR_WAKER_CPU(i), (MmioRead32 (GICR_WAKER_CPU(i)) & ~GIC_WAKER_PROCESSORSLEEP));
+      MmioWrite32 (GICR_WAKER_CPU (i), (MmioRead32 (GICR_WAKER_CPU (i)) & ~GIC_WAKER_PROCESSORSLEEP));
     }
   }
 }
--- Silicon/Qualcomm/WaipioPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
+++ Silicon/Qualcomm/WaipioPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
@@ -1,5 +1,6 @@
 #include <Library/IoLib.h>
 #include <Library/PlatformPrePiLib.h>
+#include <Library/ConfigurationMapHelperLib.h>
 #include <Library/PcdLib.h>
 
 #include "PlatformRegisters.h"
@@ -7,6 +8,15 @@
 VOID
 PlatformInitialize ()
 {
-  // Initialize GIC
-  MmioWrite32 (GICR_WAKER_CURRENT_CPU, (MmioRead32 (GICR_WAKER_CURRENT_CPU) & ~GIC_WAKER_PROCESSORSLEEP));
+  EFI_STATUS Status;
+  UINT32     EarlyInitCoreCnt;
+
+  // Get Early Cores Count
+  Status = LocateConfigurationMapUINT32ByName ("EarlyInitCoreCnt", &EarlyInitCoreCnt);
+  if (!EFI_ERROR (Status)) {
+    // Wake Up all Cores
+    for (UINTN i = 0; i < EarlyInitCoreCnt; i++) {
+      MmioWrite32 (GICR_WAKER_CPU (i), (MmioRead32 (GICR_WAKER_CPU (i)) & ~GIC_WAKER_PROCESSORSLEEP));
+    }
+  }
 }
