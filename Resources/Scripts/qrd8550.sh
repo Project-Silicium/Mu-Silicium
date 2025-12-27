400c70ac963a60a7098af0a756c9e6fbc30f1fe1
arminask <arminas@ver.lt>
2025-12-27 20:29:41 +0200

    Add qrd8550Pkg

---
+++ Resources/Scripts/qrd8550.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Build an Android kernel that is actually UEFI disguised as the Kernel
+cat ./BootShim/BootShim.bin "./Build/qrd8550Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/QRD8550_UEFI.fd" > "./Build/qrd8550Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/QRD8550_UEFI.fd-bootshim"||exit 1
+gzip -c < "./Build/qrd8550Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/QRD8550_UEFI.fd-bootshim" > "./Build/qrd8550Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/QRD8550_UEFI.fd-bootshim.gz"||exit 1
+cat "./Build/qrd8550Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/QRD8550_UEFI.fd-bootshim.gz" ./Resources/DTBs/qrd8550.dtb > ./Resources/bootpayload.bin||exit 1
+
+# Create bootable Android boot.img
+python3 ./Resources/Scripts/mkbootimg.py \
+  --kernel ./Resources/bootpayload.bin \
+  --ramdisk ./Resources/ramdisk \
+  --kernel_offset 0x00000000 \
+  --ramdisk_offset 0x00000000 \
+  --tags_offset 0x00000000 \
+  --os_version 13.0.0 \
+  --os_patch_level "$(date '+%Y-%m')" \
+  --header_version 3 \
+  -o Mu-qrd8550.img \
+  ||_error "\nFailed to create Android Boot Image!\n"
