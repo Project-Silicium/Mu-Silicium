4a68d3a779b35674bc015f70745b23c2fda6d938
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2026-01-08 15:26:19 +0100

    Kamorta: Add SMMU Detach

--- Silicon/Qualcomm/KamortaPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
+++ Silicon/Qualcomm/KamortaPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
@@ -1,11 +1,34 @@
 #include <Library/IoLib.h>
 #include <Library/PlatformPrePiLib.h>
+#include <Library/MemoryMapHelperLib.h>
+#include <Library/ArmSmmuDetachLib.h>
 
 #include "PlatformRegisters.h"
 
 VOID
+DisableWatchDogTimer ()
+{
+  EFI_STATUS                      Status;
+  EFI_MEMORY_REGION_DESCRIPTOR_EX WDogRegion;
+
+  // Locate "APSS_WDT_TMR1" Memory Region
+  Status = LocateMemoryMapAreaByName ("APSS_WDT_TMR1", &WDogRegion);
+  if (EFI_ERROR (Status)) {
+    return;
+  }
+
+  // Disable WatchDog Timer
+  MmioWrite32 (WDogRegion.Address + APSS_WDT_ENABLE_OFFSET, 0x0);
+}
+
+VOID
 PlatformInitialize ()
 {
+  CONST UINT16 MdpStreams[] = { 0x420, 0x421 };
+
   // Disable WatchDog Timer
-  MmioWrite32 (APSS_WDT_BASE + APSS_WDT_ENABLE_OFFSET, 0x0);
+  DisableWatchDogTimer ();
+
+  // Detach IOMMU Domains
+  ArmSmmuDetach (MdpStreams, ARRAY_SIZE (MdpStreams));
 }
