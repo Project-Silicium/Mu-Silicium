df1e7a4e937feaeae5b7c6fa29420a2b1f3e3495
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2026-01-31 13:45:25 +0100

    Strait: Remove non MDP MMU SIDs

--- Silicon/Qualcomm/StraitPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
+++ Silicon/Qualcomm/StraitPkg/Library/PlatformPrePiLib/PlatformPrePiLib.c
@@ -4,7 +4,7 @@
 VOID
 PlatformInitialize ()
 {
-  CONST UINT16 MdpStreams[] = { 0x820, 0x821, 0x83C, 0x83D, 0xC60, 0xC61, 0xC63, 0xC64, 0xC80 };
+  CONST UINT16 MdpStreams[] = { 0x820, 0x821, 0x83C, 0x83D };
 
   // Detach IOMMU Domains
   ArmSmmuDetach (MdpStreams, ARRAY_SIZE (MdpStreams));
