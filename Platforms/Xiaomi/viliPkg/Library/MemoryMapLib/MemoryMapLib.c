ed885836f6f6825cdf127b8fbf98d29abc9f661d
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2026-06-26 15:04:43 +0200

    vili: Increase Inital UEFI Memory
    The Old Size is not Enough, Leading to Insufficient Memory on Secure Boot Builds

--- Platforms/Xiaomi/viliPkg/Library/MemoryMapLib/MemoryMapLib.c
+++ Platforms/Xiaomi/viliPkg/Library/MemoryMapLib/MemoryMapLib.c
@@ -21,7 +21,7 @@ gMemoryDescriptor[] = {
   {"UEFI Stack",        0x9FF90000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
   {"Log Buffer",        0x9FFF7000, 0x00008000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN},
   {"Info Blk",          0x9FFFF000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN},
-  {"DXE Heap",          0xB0000000, 0x04500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
+  {"DXE Heap",          0xA0000000, 0x06500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
   {"QTEE",              0xD8800000, 0x01800000, HobOnlyNoCacheSetting, MEM_RES, UNCACHEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
   {"TZApps Reserved",   0xDA000000, 0x08800000, HobOnlyNoCacheSetting, MEM_RES, UNCACHEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
   {"DBI Dump",          0xE9600000, 0x00F00000, NoHob,  MMAP_IO, INITIALIZED, Conv,   UNCACHED_UNBUFFERED_XN},
