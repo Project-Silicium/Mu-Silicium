06d3b7f5d1f660e4b379974b960970f3af949438
Robotix <98209628+Robotix22@users.noreply.github.com>
2026-07-05 15:25:58 +0200

    FBLib: Put BPP inside Parentheses

--- Silicon/Silicium/SiliciumPkg/Library/FrameBufferSerialPortLib/FrameBuffer.h
+++ Silicon/Silicium/SiliciumPkg/Library/FrameBufferSerialPortLib/FrameBuffer.h
@@ -4,10 +4,10 @@
 //
 // Frame Buffer Details
 //
-#define FB_WIDTH    FixedPcdGet32 (PcdFrameBufferWidth)
-#define FB_HEIGHT   FixedPcdGet32 (PcdFrameBufferHeight)
-#define FB_BPP      FixedPcdGet8  (PcdFrameBufferColorDepth) / 8
-#define FB_DELAY_US FixedPcdGet32 (PcdFrameBufferDelay)
+#define FB_WIDTH     FixedPcdGet32 (PcdFrameBufferWidth)
+#define FB_HEIGHT    FixedPcdGet32 (PcdFrameBufferHeight)
+#define FB_BPP      (FixedPcdGet8  (PcdFrameBufferColorDepth) / 8)
+#define FB_DELAY_US  FixedPcdGet32 (PcdFrameBufferDelay)
 
 //
 // Frame Buffer Position
