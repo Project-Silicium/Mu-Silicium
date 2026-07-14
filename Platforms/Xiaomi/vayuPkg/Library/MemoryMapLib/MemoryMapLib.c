a83f0a2dc86e5b959207cb86477d1c4cc85cdca4
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2026-07-14 22:24:45 +0200

    vayu: Increase inital RAM
    Fixes Booting Issuess

--- Platforms/Xiaomi/vayuPkg/Library/MemoryMapLib/MemoryMapLib.c
+++ Platforms/Xiaomi/vayuPkg/Library/MemoryMapLib/MemoryMapLib.c
@@ -27,7 +27,7 @@ gMemoryDescriptor[] = {
   {"UEFI Stack",         0x9FFB0000, 0x00020000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
   {"Log Buffer",         0x9FFF7000, 0x00008000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN},
   {"Info Blk",           0x9FFFF000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, WRITE_BACK_XN},
-  {"DXE Heap",           0xA0000000, 0x03C00000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
+  {"DXE Heap",           0xA0000000, 0x06400000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
 
   // Other Memory Regions
   {"AOP_SS_MSG_RAM",     0x0C300000, 0x00100000, NoHob,  MMAP_IO, INITIALIZED, Conv,   NS_DEVICE},
