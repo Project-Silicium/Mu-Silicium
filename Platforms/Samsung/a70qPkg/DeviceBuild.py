30a50e02bd77e96ccde71671e1e026aec0427bb5
Pham Quang Minh <282817964+kdetechy-mp4@users.noreply.github.com>
2026-08-25 22:30:36 +0700

    Add files via upload

--- Platforms/Samsung/a70qPkg/DeviceBuild.py
+++ Platforms/Samsung/a70qPkg/DeviceBuild.py
@@ -35,7 +35,6 @@ class CommonPlatform ():
         "Common/Mu_OEM_Sample",
         "Mu_Basecore",
         "Silicon/Qualcomm",
-        "Silicon/Samsung",
         "Silicon/Silicium",
         "Silicium-ACPI",
         "Silicium-ACPI/Platforms/Samsung",
